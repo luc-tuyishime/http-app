f72d6dba9e884b81eb794cbc7184eb3c85854f27
luc <luctunechi45@gmail.com>
2018-10-18 01:45:41 +0200

    Toast Notification

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react"
+import { ToastContainer } from "react-toastify"
 import http from "./services/httpService"
 import config from "./config.json"
+import "react-toastify/dist/ReactToastify.css"
 import "./App.css"
 
 class App extends Component {
@@ -40,7 +42,7 @@ class App extends Component {
     this.setState({ posts }) // new post array
 
     try {
-      await http.delete(config.apiEndpoint + "/" + post.id)
+      await http.delete("s" + config.apiEndpoint + "/" + post.id)
       throw new Error("")
     } catch (ex) {
       if (ex.response && ex.response.status === 400) {
@@ -54,6 +56,7 @@ class App extends Component {
   render() {
     return (
       <React.Fragment>
+        <ToastContainer />
         <button className="btn btn-primary" onClick={this.handleAdd}>
           Add
         </button>
