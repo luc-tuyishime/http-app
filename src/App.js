7f4ae94037e8fd0b057649670af4307c756b76f3
luc <luctunechi45@gmail.com>
2018-10-18 01:34:09 +0200

    reusable http

--- src/App.js
+++ src/App.js
@@ -1,19 +1,7 @@
 import React, { Component } from "react"
-import axios from "axios"
+import http from "./services/httpService"
+import config from "./config.json"
 import "./App.css"
-
-axios.interceptors.response.use(null, error => {
-  const expectedError =
-    error.response &&
-    error.response.status >= 400 &&
-    error.response.status < 500
-  if (expectedError) return Promise.reject(error)
-  console.log("logging error", ex)
-  alert("AN unexpected error occurred")
-  return Promise.reject(error)
-})
-
-const apiEndpoint = "http://jsonplaceholder.typicode.com/posts"
 
 class App extends Component {
   state = {
@@ -22,13 +10,13 @@ class App extends Component {
 
   async componentDidMount() {
     // pending > resolved (success) or rejected (failed)
-    const { data: posts } = await axios.get(apiEndpoint)
+    const { data: posts } = await http.get(config.apiEndpoint)
     this.setState({ posts })
   }
 
   handleAdd = async () => {
     const obj = { title: "a", body: "b" }
-    const { data: post } = await axios.post(apiEndpoint, obj)
+    const { data: post } = await http.post(config.apiEndpoint, obj)
     console.log(post)
 
     const posts = [post, ...this.state.posts] // create new array and add at the beginning
@@ -37,7 +25,7 @@ class App extends Component {
 
   handleUpdate = async post => {
     post.title = "UPDATED"
-    const { data } = await axios.put(apiEndpoint + "/" + post.id, post)
+    const { data } = await http.put(config.apiEndpoint + "/" + post.id, post)
 
     const posts = [...this.state.posts] // clone all the posts
     const index = posts.indexOf(post) // find index of post in the array
@@ -52,7 +40,7 @@ class App extends Component {
     this.setState({ posts }) // new post array
 
     try {
-      await axios.delete(apiEndpoint + "/" + post.id)
+      await http.delete(config.apiEndpoint + "/" + post.id)
       throw new Error("")
     } catch (ex) {
       if (ex.response && ex.response.status === 400) {
